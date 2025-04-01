d6daf9c88a1d0a769ab88aa4592d4c96de54f620
Luis2Sosa <luismedina2320@hotmail.com>
2025-03-31 20:42:30 -0400

    Primer commit del Sistema de Soporte Automático

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sistema-soporte-automatico
+ Sistema de soporte automático simulado en consola
---
+++ soporte.py
@@ -0,0 +1,28 @@
+def soporte_automatico(opcion):
+    if opcion == "problemas de internet": return internet
+    elif opcion == "problemas con productos": return productos
+    elif opcion == "hablar con un agente": return agente
+    else:
+        return lambda: "Opcion no valida."
+    
+def internet():
+    return "Reinicie su router y vuelva a intentarlo."
+def productos():
+    return "Puede hacer la devolucion del producto en 5 dias."
+def agente():
+    return "El agente del sistema se estara comunicando con usted en un momento."    
+
+print("BIENVENIDO AL SISTEMA DE SOPORTE\n")
+while True:
+    print("\nMenu\n")
+    print("problemas de internet") 
+    print("problemas con productos")
+    print("hablar con un agente")
+    print("salir\n")        
+    
+    opcion = input("Diganos que desea realizar?\n").lower()
+    if opcion == "salir":
+        print("Gracias por confiar en nosotros. Un placer asistirle.")
+        break
+    soporte = soporte_automatico(opcion)
+    print(soporte())            
